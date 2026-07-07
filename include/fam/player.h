28bc013764bbf72eb7860a922fe4b3b0b2b26967
nekromantikko <jonah.paakkonen@gmail.com>
2026-07-07 22:18:11 +0300

    Change player init API to take APU as parameter

--- include/fam/player.h
+++ include/fam/player.h
@@ -7,7 +7,7 @@ typedef struct FamMusic FamMusic;
 typedef struct FamSfx FamSfx;
 typedef struct FamPlayer FamPlayer;
 
-FamResult fam_player_init(FamPlayer** out_player, uint32_t sample_rate);
+FamResult fam_player_init(FamPlayer** out_player, FamApu* apu, uint32_t sample_rate);
 void fam_player_free(FamPlayer* player);
 void fam_player_process_samples(FamPlayer* player, int count, void* out_samples);
 
