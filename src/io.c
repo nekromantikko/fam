74f676c9b7ad775576faa4cb08c574ba28fdeec6
nekromantikko <jonah.paakkonen@gmail.com>
2026-07-29 11:39:16 +0300

    Extract BufferReader into its own file

--- src/io.c
+++ src/io.c
@@ -1,5 +1,6 @@
 #include <fam/io.h>
 #include <fam/internal/stream_types.h>
+#include <fam/internal/buffer_reader.h>
 #include <stdlib.h>
 #include <stdbool.h>
 #include <string.h>
@@ -12,77 +13,6 @@ typedef enum {
     FAM_USAGE_MUSIC = 0,
     FAM_USAGE_SFX,
 } FamUsage;
-
-// TODO: Move to utils
-typedef struct {
-    uint8_t* const start;
-    uint8_t* pos;
-    uint8_t* const end;
-    bool error;
-} BufferReader;
-
-static inline BufferReader buffer_reader_init(const void* buffer, size_t size) {
-    BufferReader reader = {
-        .start = (uint8_t*)buffer,
-        .pos = (uint8_t*)buffer,
-        .end = (uint8_t*)buffer + size,
-        .error = false
-    };
-    return reader;
-}
-
-static inline void buffer_reader_read(BufferReader* reader, void* dst, size_t count) {
-    if (reader->error) {
-        return;
-    }
-
-    // Trying to read past block end
-    if (count > (size_t)(reader->end - reader->pos)) {
-        reader->pos = reader->end;
-        reader->error = true;
-        return;
-    }
-
-    memcpy(dst, (void*)reader->pos, count);
-    reader->pos += count;
-}
-
-static inline void buffer_reader_skip(BufferReader* reader, size_t count) {
-    if (reader->error) {
-        return;
-    }
-
-    // Trying to skip past block end
-    if (count > (size_t)(reader->end - reader->pos)) {
-        reader->pos = reader->end;
-        reader->error = true;
-        return;
-    }
-
-    reader->pos += count;
-}
-
-static inline void buffer_reader_seek(BufferReader* reader, size_t offset) {
-    if (reader->error) {
-        return;
-    }
-
-    if (offset > (size_t)(reader->end - reader->start)) {
-        reader->pos = reader->end;
-        reader->error = true;
-        return;
-    }
-
-    reader->pos = reader->start + offset;
-}
-
-static inline size_t buffer_reader_size(const BufferReader* reader) {
-    return (size_t)(reader->end - reader->start);
-}
-
-static inline size_t buffer_reader_remaining(const BufferReader* reader) {
-    return (size_t)(reader->end - reader->pos);
-}
 
 static FamResult validate_header(BufferReader* reader, FamUsage expected_usage) {
     uint32_t magic;
